41497df08f8e567ee6b81b9b5962d0e4296789f5
Derek Wilson <mail@derekwilson.net>
2017-07-04 19:24:40 +1200

    First couple of unit tests for the controller

---
+++ UnitTests/ProductControllerTests/BaseProductControllerTests.cs
@@ -0,0 +1,65 @@
+﻿using Domain.Model;
+using Domain.Repository;
+using Moq;
+using NUnit.Framework;
+using refactor_me.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests.ProductControllerTests
+{
+	class BaseProductControllerTests
+	{
+		protected ProductsController _controller;
+		protected Mock<IProductRepository> _mockProductRepository;
+		protected Mock<IProductOptionRepository> _mockProductOptionRepository;
+
+		protected static Guid TestProductId1 = new Guid("11111111-1111-1111-2222-111111111111");
+		protected static Guid TestProductId2 = new Guid("22222222-1111-1111-2222-111111111111");
+		protected static Guid TestProductInvalidId = new Guid("33333333-1111-1111-2222-111111111111");
+
+		protected IList<Product> _testProductData;
+
+		private IList<Product> GetProductTestsData()
+		{
+			return new List<Product>(10)
+			{
+				new Product {
+					Id = TestProductId1,
+					Name = "TestName1",
+					Description = "TestDescription",
+					DeliveryPrice = 1,
+					Price = 2
+				},
+				new Product {
+					Id = TestProductId2,
+					Name = "TestName2",
+					Description = "TestDescription",
+					DeliveryPrice = 1,
+					Price = 2
+				}
+			};
+		}
+
+		[SetUp]
+		public void TestSetup()
+		{
+			_testProductData = GetProductTestsData();
+			_mockProductRepository = new Mock<IProductRepository>();
+			_mockProductOptionRepository = new Mock<IProductOptionRepository>();
+
+			_mockProductRepository.Setup(productRepo => productRepo.GetAll()).Returns(_testProductData);
+
+			_controller = new ProductsController(_mockProductRepository.Object, _mockProductOptionRepository.Object);
+		}
+
+		[TearDown]
+		public void TestTearDown()
+		{
+			_controller = null;
+		}
+	}
+}
---
+++ UnitTests/ProductControllerTests/GetProductTests.cs
@@ -0,0 +1,46 @@
+﻿using Domain.Model;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+
+namespace UnitTests.ProductControllerTests
+{
+	[TestFixture]
+	class GetProductTests : BaseProductControllerTests
+	{
+		[Test]
+		public void ShouldReturnDataWhenFound()
+		{
+			// Arrange
+			_mockProductRepository.Setup(productRepo => productRepo.GetById(It.IsAny<Guid>())).Returns(_testProductData.First());
+
+			// Act
+			var httpActionResult = _controller.GetProduct(TestProductId1);
+
+			// Assert
+			OkNegotiatedContentResult<Product> contentResult = httpActionResult as OkNegotiatedContentResult<Product>;
+			Assert.IsNotNull(contentResult);
+			Assert.AreEqual(contentResult.Content.Name, "TestName1");
+		}
+
+		[Test]
+		public void ShouldReturnErrorWhenNotFound()
+		{
+			// Arrange
+
+			// Act
+			var httpActionResult = _controller.GetProduct(TestProductInvalidId);
+
+			// Assert
+			StatusCodeResult contentResult = httpActionResult as StatusCodeResult;
+			Assert.IsNotNull(contentResult);
+			Assert.AreEqual(HttpStatusCode.NotFound, contentResult.StatusCode);
+		}
+	}
+}
